20e35d8a82f8b77e4d8a0d3b37b1063d682dfdee
Seth Tanner <101337407+SethT6720@users.noreply.github.com>
2026-01-08 17:04:07 +0000

    Co-authored-by: sadturtle00 <sadturtle00@users.noreply.github.com>

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import http from 'node:http';
+import { Server } from 'socket.io';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+
+const io = Server(server);
+
+app.get('/', (req, res) => {
+    res.send('Server is up');
+});
+
+io.on('connection', (socket) => {
+    console.log("a user connected");
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+    });
+});
+
+server.listen(5000, () => {
+    console.log('server running on port 5000');
+});
